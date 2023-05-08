d9a32cdfb358182d0dbebf3085d2e69c531342d4
xYerMikx <yermik2014@yandex.ru>
2023-05-08 16:04:53 +0300

    feat: add 03-files-in-folder solution

---
+++ 03-files-in-folder/index.js
@@ -0,0 +1,23 @@
+const { dir } = require("console")
+const { read, readdir } = require("fs")
+const fs = require("fs/promises")
+const path = require("path")
+const { constrainedMemory } = require("process")
+
+const dirPath = path.join(__dirname, 'secret-folder')
+
+async function readDir(dirPath) {
+    const files = await fs.readdir(dirPath, { withFileTypes: true })
+    for (const file of files) {
+        if (file.isFile()) {
+            const fileName = file.name
+            const fileExt = path.extname(fileName).slice(1)
+            const stats = await fs.stat(path.join(dirPath, fileName))
+            const fileSize = Math.round(stats.size / 1024 * 1000) / 1000
+
+            console.log(`\n${fileName} - ${fileExt} - ${fileSize}` + " kB")
+        }
+    }
+}
+
+readDir(dirPath)
