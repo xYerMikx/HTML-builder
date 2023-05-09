ba2236255b65308dbebea5c9be4a9727b3365fba
xYerMikx <yermik2014@yandex.ru>
2023-05-09 17:13:56 +0300

    fix: fix 03-files-in-folder solution

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -8,11 +8,13 @@ async function readDir(dirPath) {
   for (const file of files) {
     if (file.isFile()) {
       const fileName = file.name;
+      const periodIndex = fileName.indexOf('.');
+      const fileWithoutExt = fileName.slice(0, periodIndex);
       const fileExt = path.extname(fileName).slice(1);
       const stats = await fs.stat(path.join(dirPath, fileName));
       const fileSize = Math.round(stats.size / 1024 * 1000) / 1000;
 
-      console.log(`\n${fileName} - ${fileExt} - ${fileSize}` + ' kB');
+      console.log(`\n${fileWithoutExt} - ${fileExt} - ${fileSize}` + ' kB');
     }
   }
 }
